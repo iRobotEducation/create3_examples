b27b70cb3c7d1a3aaef6ba67e197b3ca886f574f
Steven Shamlian <sshamlian@irobot.com>
2022-05-18 10:07:34 -0400

    remove unused dep

--- create3_lidar/launch/sensors_launch.py
+++ create3_lidar/launch/sensors_launch.py
@@ -5,7 +5,6 @@ from launch import LaunchContext
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import math
 
 def generate_launch_description():
     return LaunchDescription([
