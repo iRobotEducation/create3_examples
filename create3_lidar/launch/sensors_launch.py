5a7e4f3fe03f66ddaa6b2a3a671ddfa129cf2554
Briana Bouchard <briana.bouchard@tufts.edu>
2023-07-21 11:38:55 -0400

    Replaces node name with wildcard syntax in yaml
    Replacement needed to enable function with namespace

--- create3_lidar/launch/sensors_launch.py
+++ create3_lidar/launch/sensors_launch.py
@@ -16,25 +16,36 @@ def generate_launch_description():
         default_value='',
         description='Robot namespace')
     
+    # Declares an action that will launch a node when executed by the launch description.
+    # This node is responsible for providing a static transform from the robot's base_footprint
+    # frame to a new laser_frame, which will be the coordinate frame for the lidar. 
     static_transform_node = Node(
         package='tf2_ros', 
         executable='static_transform_publisher',
         arguments=['-0.012', '0', '0.144', '0', '0', '0', 'base_footprint', 'laser_frame'],
+        
+        # Remaps topics used by the 'tf2_ros' package from absolute (with slash) to relative (no slash).
+        # This is necessary to use namespaces with 'tf2_ros'.
         remappings=[
             ('/tf_static', 'tf_static'),
             ('/tf', 'tf')],
         namespace=namespace
-        )
+    )
     
+    # Declares an action that will launch a node when executed by the launch description.
+    # This node is responsible for configuring the RPLidar sensor.    
     rplidar_node = Node(
+        name='rplidar_composition',
         package='rplidar_ros',
-        parameters=[
-                get_package_share_directory("create3_lidar") + '/config/rplidar_node.yaml'
-                ],
         executable='rplidar_composition',
+        output='screen',
+        parameters=[
+            get_package_share_directory("create3_lidar") + '/config/rplidar_node.yaml'
+            ],
         namespace=namespace
-        )
+    )
     
+    # Launches all named actions
     return LaunchDescription([
         namespace_argument,
         static_transform_node,
